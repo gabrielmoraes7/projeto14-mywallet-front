a48f60de8a5073a9124d36dda4f11e93aa85b5a6
gabrielmoraes7 <gabriel.moclemente7@gmail.com>
2024-01-08 04:13:26 -0300

    inicialização tardia

---
+++ src/pages/EditTransactionPage.jsx
@@ -0,0 +1,106 @@
+import { useEffect, useRef, useState } from "react";
+import { useLocation, useNavigate, useParams } from "react-router-dom"
+import styled from "styled-components"
+import { EditTransaction } from "../requests";
+import { ThreeDots } from 'react-loader-spinner';
+import { toast } from "react-toastify";
+import {IoIosReturnLeft} from 'react-icons/io';
+
+export default function EditTransactionPage() {
+
+  const {tipo,id} = useParams(); // in || out
+  const navigate = useNavigate();
+  const [value,setValue]  = useState('');
+  const description  = useRef();
+  const location = useLocation();
+  //const [id,setId] = useState();
+  const [loading,setLoading] = useState(false);
+
+  useEffect(()=>{
+    if(localStorage.getItem('token') == undefined) return navigate('/');
+
+    setValue(location.state.split(',')[0]);
+    //setId(id);
+    description.current.value = location.state.split(',')[1];
+  },[])
+
+  function editTransactionEvent(e)
+  {
+    e.preventDefault();
+    setLoading(true);
+    EditTransaction(localStorage.getItem('token'),transactionSucess,id,{value,description:description.current.value},tipo);
+  }
+
+  function transactionSucess(message,error)
+  {
+    setLoading(false);
+    if(error) return alert(message.response.data);
+    toast.success( 'Sucesso ao editar transação!', {
+      position: "top-center",
+      autoClose: 1000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: false,
+      progress: undefined,
+      theme: "colored",
+      });
+    navigate('/home');
+  }
+
+  return (
+    <TransactionsContainer type={tipo}>
+      <h1>Editar {tipo == 'entrada'? 'entrada' : 'saída'}</h1>
+      <form onSubmit={editTransactionEvent}>
+        <input data-test="registry-amount-input" value={value} onChange={(e)=> {setValue(e.target.value.replace(/[^0-9.-]/g, ''))}} required placeholder="Valor" type="text"/>
+        <input data-test="registry-name-input" ref={description} required placeholder="Descrição" type="text" />
+        <button disabled={loading} className="update-btn" data-test="registry-save" >{loading && <ThreeDots color="rgba(255, 255, 255, 1)" height={13} width={51} />}{!loading && 'Atualizar '}{!loading && (tipo == 'entrada'? 'entrada' : 'saída')}</button>
+      </form>
+      <button className="back-btn" onClick={()=> navigate('/home')}><IoIosReturnLeft/></button>
+    </TransactionsContainer>
+  )
+}
+
+const TransactionsContainer = styled.main`
+  height: calc(100vh - 50px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+
+ 
+  .back-btn{
+    position: fixed;
+    top: 20px;
+    right: 25px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    border-radius: 50%;
+  }
+
+
+  .ilustration{
+    width: 100%;
+    margin-top: 100px;
+    margin-top: ${(props) => props.type == 'entrada' ? '80px' : '100px'};
+    position: fixed;
+    bottom: 1px;
+    max-height: 525px;
+  }
+
+  h1 {
+    align-self: flex-start;
+    margin-bottom: 40px;
+  }
+
+  form {
+    .update-btn{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 100%;
+    }
+  }
+`
---
+++ src/pages/SignInPage.jsx
@@ -0,0 +1,85 @@
+import styled from "styled-components";
+import { Link, useNavigate } from "react-router-dom";
+import MyWalletLogo from "../components/Logo";
+import { useRef, useState } from "react";
+import { Login } from "../requests";
+import { ThreeDots } from 'react-loader-spinner';
+
+export default function SignInPage() {
+
+  const email  = useRef();
+  const password  = useRef();
+  const navigate = useNavigate();
+  const [loading,setLoading] = useState(false);
+
+  function login(e)
+  {
+    e.preventDefault();
+
+    const userObj = {
+      email:email.current.value.toString(),
+      password:password.current.value.toString()
+    }
+    setLoading(true);
+    Login(userObj,loginSucess);
+  }
+
+  function loginSucess(message,error)
+  {
+    setLoading(false);
+    if(error)
+    {
+      console.log(message);
+      return alert(message.response.data.message);
+    }
+
+    localStorage.setItem('token',message.data.token);
+    navigate('/home',{state:`${message.data.name},${message.data.balance}`});
+  }
+
+  return (
+    <SingInContainer>
+      <form onSubmit={login}>
+        <MyWalletLogo />
+          <input data-test="email" required placeholder="E-mail" type="email" ref={email} autoComplete="true" name="email" id="email"/>
+          <input data-test="password" required placeholder="Senha" type="password" autoComplete = 'true' ref={password} name="password" id="password"/>
+          <button disabled={loading} className="sign-in-btn" data-test="sign-in-submit">{loading && <ThreeDots color="rgba(255, 255, 255, 1)" height={13} width={51} />}{!loading && 'Entrar'}</button>
+      </form>
+    <img className="background" src={signupImage} alt="" />
+      <Link to={'/cadastro'}>
+        Primeira vez? Cadastre-se!
+      </Link>
+    </SingInContainer>
+  )
+}
+
+const SingInContainer = styled.section`
+  height: calc(100vh - 50px);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  form {
+    .sign-in-btn{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 100%;
+    }
+    z-index: 1;
+  }
+
+  a{
+    z-index: 1;
+  }
+
+  .background{
+    position: fixed;
+    height: 90%;
+    z-index: 0;
+    opacity: 30%;
+    bottom: 0;
+    pointer-events: none;
+    user-select: none;
+  }
+`
